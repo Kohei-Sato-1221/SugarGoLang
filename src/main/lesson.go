ffdd10ab520a8198f21bad06519a25282df00fdb
kohei.sato@okcoin.com <kohei.sato@okcoin.com>
2019-03-27 09:42:00 +0900

    git_20190327

--- src/main/lesson.go
+++ src/main/lesson.go
@@ -11,7 +11,29 @@ import (
 )
 
 func main() {
-	lesson36()
+	lesson37()
+}
+
+func lesson37(){
+	v := Vertex{3, 4, ""}
+//	fmt.Println(CalcArea(v))
+	v.Scale(10)
+	fmt.Println(v.Area())
+}
+
+// これにより v.Area()みたいな感じで呼び出すことができる
+func (v Vertex) Area() int{
+	return v.X * v.Y
+}
+
+// pointer receiver
+func (v *Vertex) Scale(i int){
+	v.X = v.X * i
+	v.Y = v.Y * i
+}
+
+func CalcArea(v Vertex) int{
+	return v.X * v.Y
 }
 
 // 20190326 Struct

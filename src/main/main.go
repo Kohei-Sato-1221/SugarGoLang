08fb02607909f5ee9c48b34ec45317c35efd3dab
佐藤幸平 <Kohei-Sato-1221>
2019-06-05 23:17:20 +0900

    lesson79

--- src/main/main.go
+++ src/main/main.go
@@ -1,114 +1,76 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
-	
-	"golang.org/x/sync/semaphore"
-	"gopkg.in/ini.v1"
-	
 	"log"
-	"net/url"
-	"github.com/gorilla/websocket"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 
 func main(){
-	lesson78()
+	lesson79()
 }
 
-type JsonRPC2 struct {
-	Version string      `json:"jsonrpc"`
-	Method  string      `json:"method"`
-	Params  interface{} `json:"params"`
-	Result  interface{} `json:"result,omitempty"`
-	Id      *int        `json:"id,omitempty"`
+var DbConnection *sql.DB
+
+type Person struct {
+	Name string
+	Age int
 }
 
-type SubscribeParams struct {
-	Channel string `json:"channel"`
-}
-
-func lesson78(){
-	u := url.URL{Scheme: "wss", Host: "ws.lightstream.bitflyer.com", Path: "/json-rpc"}
-	log.Printf("connectiong to %s", u.String())
-	
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+func lesson79(){
+	DbConnection, _ := sql.Open("sqlite3", "./example.sql")
+	defer DbConnection.Close()
+	cmd := `CREATE TABLE IF NOT EXISTS person(name STRING, age INT)`
+	_, err := DbConnection.Exec(cmd)
 	if err != nil {
-		log.Fatal("dial:", err)
-	}
-	defer c.Close()
-	
-	if err := c.WriteJSON(&JsonRPC2{Version: "2.0", Method: "subscribe", Params: &SubscribeParams{"lightning_ticker_BTC_JPY"}}); err != nil {
-		log.Fatal("subscribe:", err)
-		return
+		log.Fatalln(err)
 	}
 	
-	for {
-		message := new(JsonRPC2)
-		if err := c.ReadJSON(message); err != nil {
-			log.Println("read:", err)
-			return
-		}
-		
-		if message.Method == "channelMessage" {
-			log.Println(message.Params);
-		}
-	}
-}
-
-type ConfigList struct{
-	Port      int
-	DbName    string
-	SQLDriver string
-}
-
-var Config ConfigList
-
-func init(){
-	cfg, _ := ini.Load("config.ini")
-	Config = ConfigList{
-		Port: cfg.Section("web").Key("port").MustInt(),
-		DbName: cfg.Section("db").Key("name").MustString("example.sql"),
-		SQLDriver: cfg.Section("db").Key("driver").String(),
-	}
-}
-
-// go ini → configファイルをよむライブラリ
-func lesson77(){
-	fmt.Printf("%T %v\n", Config.Port, Config.Port)
-	fmt.Printf("%T %v\n", Config.DbName, Config.DbName)
-	fmt.Printf("%T %v\n", Config.SQLDriver, Config.SQLDriver)
-}
-
-// Semaphore →　同時に実行されるgoroutineの数を制御する
-func lesson76(){
-	ctx := context.TODO()
-	go longProcess(ctx)	
-	go longProcess(ctx)	
-	go longProcess(ctx)
-	time.Sleep(5 * time.Second)
-}
-
-var s *semaphore.Weighted = semaphore.NewWeighted(1)
-
-func longProcess(ctx context.Context){
-	isAcquire := s.TryAcquire(1)
-	if !isAcquire {
-		fmt.Println("Could not get lock....")
-		return
-	}
-	
-//	if err := s.Acquire(ctx, 1); err != nil{
-//		fmt.Print(err)
-//		return
+//	cmd = "INSERT INTO person (name, age) VALUES (?, ?)"
+//	_, err = DbConnection.Exec(cmd, "Kohei", 31)
+//	_, err = DbConnection.Exec(cmd, "Kanako", 28)
+//	if err != nil {
+//		log.Fatal(err)
 //	}
-	defer s.Release(1)
-	fmt.Println("Wait......")
-	time.Sleep(1 * time.Second)
-	fmt.Println("Done")	
+
+//	cmd = "UPDATE person SET age =? WHERE name = ?"
+//	_, err = DbConnection.Exec(cmd, 100, "Kanako")
+//	if err != nil {
+//		log.Fatalln(err)
+//	}
+
+//	cmd = "SELECT * FROM person"
+//	rows, _ := DbConnection.Query(cmd)
+//	defer rows.Close()
+//	var pp []Person
+//	for rows.Next() {
+//		var p Person
+//		err := rows.Scan(&p.Name, &p.Age)
+//		if err != nil {
+//			log.Println(err)
+//		}
+//		pp = append(pp, p)
+//	}
+//	for _, p := range pp {
+//		fmt.Println(p.Name, p.Age)
+//	}
+	
+	cmd = "SELECT * FROM person where age = ?"
+	row := DbConnection.QueryRow(cmd, 1000)
+	var p Person
+	err = row.Scan(&p.Name, &p.Age)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Println("No row")
+		} else {
+			log.Println(err)
+		}
+	}
+	fmt.Println(p.Name, p.Age)
 }
+
 
 
 

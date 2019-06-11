8d22a116a473b03ab3ee308eb2a3973d00fd497d
Kohei-Sato-1221 <kohei.sato@okcoin.com>
2019-06-11 09:33:49 +0900

    lesson86

--- src/main/main.go
+++ src/main/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"io/ioutil"
 	"fmt"
+	"net/http"
+	"log"
 )
 
 type Page struct{
@@ -24,10 +26,21 @@ func loadPage(title string) (*Page, error){
 	return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request){
+	// rにはアクセスした情報（httpリクエストの内容）が含まれる
+	// wにはレスポンスに加える内容を付加する
+	title := r.URL.Path[len("/view/"):]
+	fmt.Println(title)
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main(){
-	p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
-	p1.save()
-	
-	p2, _ := loadPage(p1.Title)
-	fmt.Println(string(p2.Body))
+//	p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
+//	p1.save()
+//	
+//	p2, _ := loadPage(p1.Title)
+//	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }

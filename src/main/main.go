a1c36dadb0b6a912b7c4aaba6694ff5e90c1f89a
佐藤幸平 <Kohei-Sato-1221>
2019-06-15 14:06:07 +0900

    87

--- src/main/main.go
+++ src/main/main.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"fmt"
 	"net/http"
+	"html/template"
 	"log"
 )
 
@@ -19,28 +20,48 @@ func (p *Page) save() error {
 
 func loadPage(title string) (*Page, error){
 	filename := title + ".txt"
+	fmt.Printf(filename)
 	body, err := ioutil.ReadFile(filename)
 	if err != nil{
+		fmt.Printf("No file.... " + filename)
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request){
 	// rにはアクセスした情報（httpリクエストの内容）が含まれる
 	// wにはレスポンスに加える内容を付加する
-	title := r.URL.Path[len("/view/"):]
+//	title := r.URL.Path[len("/view/"):]
+	title := "view"
 	fmt.Println(title)
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/edit/"):]
+	fmt.Println(title)
+	p, err := loadPage(title)
+	
+	if err != nil{
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
 }
 
 func main(){
-//	p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
-//	p1.save()
+	p1 := &Page{Title: "view", Body: []byte("This is a sample Page.")}
+	p1.save()
 //	
 //	p2, _ := loadPage(p1.Title)
 //	fmt.Println(string(p2.Body))
 	http.HandleFunc("/view/", viewHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/edit/", editHandler)
+	log.Fatal(http.ListenAndServe(":8090", nil))
 }
